5ca5e9ae21bc50ae5dec5de0593ae3fd83307ef7
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-04 10:52:28 +0300

    Update api.py

--- services/api.py
+++ services/api.py
@@ -1,1 +1,14 @@
+import aiohttp
 
+from config import BACKEND_URL
+
+
+async def get_wallet(telegram_id: int):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            f"{BACKEND_URL}/wallet/{telegram_id}"
+        ) as response:
+            if response.status != 200:
+                return None
+
+            return await response.json()
