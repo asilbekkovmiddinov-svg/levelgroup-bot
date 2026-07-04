51f54b0fea5083ca95a7a56fd42304527ad48f50
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-04 09:42:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BACKEND_URL = os.getenv("BACKEND_URL")
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN topilmadi!")
+
+if not BACKEND_URL:
+    raise ValueError("BACKEND_URL topilmadi!")
