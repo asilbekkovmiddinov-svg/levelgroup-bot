a0e0143ea18fc948b28d03bb95461df5dbcb3a25
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-04 09:46:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
+import asyncio
 
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import BOT_TOKEN
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML
+    )
+)
+
+dp = Dispatcher()
+
+
+async def main():
+    print("🚀 LEVEL_GROUP Bot ishga tushdi...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
