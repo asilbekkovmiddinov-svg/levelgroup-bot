d4d8f784cfb6dfd29f01290bcbba7331452d7554
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-04 10:25:51 +0300

    Create menu.py

---
+++ keyboards/menu.py
@@ -0,0 +1,19 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="💰 Hamyon"),
+            KeyboardButton(text="🛒 Coin sotib olish")
+        ],
+        [
+            KeyboardButton(text="🤝 P2P Market"),
+            KeyboardButton(text="🎰 Baraban")
+        ],
+        [
+            KeyboardButton(text="👤 Profil"),
+            KeyboardButton(text="⚙️ Sozlamalar")
+        ]
+    ],
+    resize_keyboard=True
+)
