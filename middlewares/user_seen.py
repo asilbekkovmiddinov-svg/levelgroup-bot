ea76efe04fa38ec41e3ae2363b33524c75141b2e
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-07 10:24:26 +0300

    Update user_seen.py

--- middlewares/user_seen.py
+++ middlewares/user_seen.py
@@ -1,1 +1,24 @@
+from typing import Any, Awaitable, Callable, Dict
 
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+
+from services.api import update_user_seen
+
+
+class UserSeenMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        user = data.get("event_from_user")
+
+        if user:
+            try:
+                await update_user_seen(user.id)
+            except Exception:
+                pass
+
+        return await handler(event, data)
