0ce331f76f3fb11cacadf683d633aeef4305202e
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-07-11 19:24:02 +0300

    Secure wallet API integration

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 BACKEND_URL = os.getenv("BACKEND_URL")
+INTERNAL_API_KEY = os.getenv("INTERNAL_API_KEY")
 
 NEW_ORDERS_CHANNEL_ID = int(os.getenv("NEW_ORDERS_CHANNEL_ID", "0"))
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
