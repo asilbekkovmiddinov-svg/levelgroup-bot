f486c30ac534e86c4b9d05f060725ab2c2c0d827
asilbekkovmiddinov-svg <asilbekkovmiddinov@gmail.com>
2026-08-27 20:36:01 +0300

    feat: register channel game post router

--- bot.py
+++ bot.py
@@ -20,6 +20,7 @@ from handlers.admin_arena_v4 import router as admin_arena_v4_router
 from handlers.admin_coin_chat import router as admin_coin_chat_router
 from handlers.admin_coin_shop import router as admin_coin_shop_router
 from handlers.admin_shop import router as admin_shop_router
+from handlers.admin_channel_post import router as admin_channel_post_router
 from handlers.arena_relay import router as arena_relay_router
 
 from middlewares.user_seen import UserSeenMiddleware
@@ -60,6 +61,7 @@ dp.include_router(admin_arena_v4_router)
 dp.include_router(admin_coin_chat_router)
 dp.include_router(admin_coin_shop_router)
 dp.include_router(admin_shop_router)
+dp.include_router(admin_channel_post_router)
 dp.include_router(arena_relay_router)
 
 
@@ -186,13 +188,10 @@ async def arena_ready_finish_worker():
 
 
 async def main():
-    print("🚀 LEVEL_GROUP Bot ishga tushdi...")
-
     asyncio.create_task(p2p_timeout_worker())
     asyncio.create_task(arena_ready_start_worker())
     asyncio.create_task(arena_ready_finish_worker())
-    asyncio.create_task(CampaignDeliveryWorker(bot).run())
-
+    asyncio.create_task(CampaignDeliveryWorker(bot).run_forever())
     await dp.start_polling(bot)
 
 
